7509c60c21be9cc8a49cb68d31f0320fe9651236
Rishabh Gupta <rishav.mh103@gmail.com>
2025-01-02 11:50:36 +0530

    Commit

---
+++ src/test/java/com/company/fileSharingManagement/FileSharingManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.company.fileSharingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class FileSharingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
